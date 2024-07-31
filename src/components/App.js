a6e90076a12ac818dce79f6606d79e8b1b4db267
Daniel Watoro <watorondirangu@gmail.com>
2024-07-31 21:49:45 +0300

    Adds a new component

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <ul>
+        <a href="#home">I'm a link!</a>
+      </ul>
     </nav>
   );
 }
@@ -17,14 +19,21 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h2>About.</h2>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
